fe02a9f1c82d544e19185be1fc37fecee485a340
216037150 <siyabongazungu95@gmail.com>
2024-12-17 10:32:47 +0200

    created password, now I wil add logic base on, the length of password, use of numbers, special charecters, and uppercase letters

--- PassWord.java
+++ PassWord.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
+/*
+ * Create a program that checks if a given password is strong based on criteria like:
+ *  length, use of numbers, special characters, and uppercase letters.
+ */
 class PassWord{
+
     public static void main(String[] args){
 
         Scanner password = new Scanner(System.in);
+        System.out.println("Please create a strong password...");
+        String myPassword = password.nextLine();
+        
 
     }
 }
